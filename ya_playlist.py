955d725b5596033ea692bf48b32963e2b02d3c38
Vitaliy Inyushev <vinyushev@mail.ru>
2022-06-04 14:22:25 +0300

    added the function of getting a list of tracks from yandex music

---
+++ ya_playlist.py
@@ -0,0 +1,16 @@
+from yandex_music import Client
+from settings import token
+
+
+def get_playlist(url):
+    user_name = url.split('/')[4] 
+    id_playlist = url.split('/')[-1]
+    playlist_list = Client().users_playlists(int(id_playlist), user_name)
+    g = playlist_list['tracks']
+    print(type(g[0]))
+    h = g[0]
+    
+    return h['id']
+    
+
+print(get_playlist('https://music.yandex.ru/users/shamantur116/playlists/1009'))    
