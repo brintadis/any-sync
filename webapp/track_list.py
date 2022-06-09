fdaf7576afca5aae57eef1125cc66ae82d75a6a8
Vitaliy Inyushev <vinyushev@mail.ru>
2022-06-09 19:16:50 +0300

    вывел список треков на отдельную страницу

--- webapp/track_list.py
+++ webapp/track_list.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+from ya_playlist import name_tracks, artists_tracks, duration_tracks
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     title = "Список треков"
-    return render_template('track_list.html', page_title=title)
+    name = name_tracks
+    artist = artists_tracks
+    duration = duration_tracks
+    return render_template('track_list.html', page_title=title, name=name, artist=artist, duration=duration)
 
 if __name__ == "__main__":
     app.run(debug=True)
