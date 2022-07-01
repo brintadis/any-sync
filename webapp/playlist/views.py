6f0e0019e5f02f2f2ed3d8caa073b040b454bf17
Makar Timofeev <75175813+brintadis@users.noreply.github.com>
2022-07-01 17:40:02 +0300

    login required added to main playlist page

--- webapp/playlist/views.py
+++ webapp/playlist/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash
+from flask import Blueprint
 from flask import render_template, redirect
 from flask_login import current_user, login_required
 
@@ -12,20 +12,18 @@ blueprint = Blueprint('playlist', __name__, url_prefix='/playlist')
 
 
 @blueprint.route("/", methods=['GET', 'POST'])
+@login_required
 def test():
     title = "AnySync"
     url_form = PlaylistLinkForm()
 
-    try:
-        if url_form.validate_on_submit():
-            if 'spotify' in url_form.link.data:
-                new_playlist = get_playlist_by_id(url_form.link.data)
-                return redirect(f'playlist/{new_playlist.id}')
-            elif 'yandex' in url_form.link.data:
-                new_playlist = get_playlist_ya(url_form.link.data)
-                return redirect(f'playlist/{new_playlist.id}')
-    except AttributeError:
-        flash('Авторизуйтесь прежде чем добавлять плейлист')
+    if url_form.validate_on_submit():
+        if 'spotify' in url_form.link.data:
+            new_playlist = get_playlist_by_id(url_form.link.data)
+            return redirect(f'playlist/{new_playlist.id}')
+        elif 'yandex' in url_form.link.data:
+            new_playlist = get_playlist_ya(url_form.link.data)
+            return redirect(f'playlist/{new_playlist.id}')
 
     return render_template(
         'playlist/test.html',
