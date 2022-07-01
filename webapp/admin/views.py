2401b1b444c8f457f0b7f470cc679be9353a677f
Makar Timofeev <75175813+brintadis@users.noreply.github.com>
2022-07-01 22:50:09 +0300

    playlists count loop moved to a models

--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template
 
-from webapp.playlist.models import Playlist
 from webapp.user.models import User
 from webapp.user.decorators import admin_required
 
@@ -18,17 +17,9 @@ def admin_index():
 def admin_playlists():
     title = 'Плейлисты пользователей'
     users = User.query.all()
-    playlists_count = []
-    for user in users:
-        playlists_count.append(
-            Playlist.query.filter(
-                Playlist.user == user.id
-            ).count()
-        )
 
     return render_template(
         'admin/all_users_playlists.html',
         title=title,
-        playlists_count=playlists_count,
         users=users
     )
--- webapp/user/models.py
+++ webapp/user/models.py
@@ -1,7 +1,9 @@
 from flask_login import UserMixin
+
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from webapp.db import db
+from webapp.playlist.models import Playlist
 
 
 class User(db.Model, UserMixin):
@@ -10,6 +12,9 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(128))
     role = db.Column(db.String(10), index=True)
     email = db.Column(db.String(50))
+
+    def __repr__(self):
+        return '<User {} id {}>'.format(self.username, self.id)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -21,5 +26,5 @@ class User(db.Model, UserMixin):
     def is_admin(self):
         return self.role == 'admin'
 
-    def __repr__(self):
-        return '<User {} id {}>'.format(self.username, self.id)
+    def count_playlist(self):
+        return Playlist.query.filter(Playlist.user == self.id).count()
