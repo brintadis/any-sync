42c0f167a1e231b70f5670a5e0437e30fd0fafd1
Makar Timofeev <75175813+brintadis@users.noreply.github.com>
2022-07-02 16:57:16 +0300

    admin panel redeisigned

--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -3,23 +3,23 @@ from flask import Blueprint, render_template
 from webapp.user.models import User
 from webapp.user.decorators import admin_required
 
+from webapp.playlist.models import Playlist
+
 blueprint = Blueprint('admin', __name__, url_prefix='/admin')
 
 
 @blueprint.route('/')
 @admin_required
 def admin_index():
-    return 'Привет админ'
-
-
-@blueprint.route('/user-playlists')
-@admin_required
-def admin_playlists():
-    title = 'Плейлисты пользователей'
+    title = 'Админ панель'
     users = User.query.all()
+    users_count = User.query.count()
+    playlists_count = Playlist.query.count()
 
     return render_template(
-        'admin/all_users_playlists.html',
+        'admin/index.html',
         title=title,
-        users=users
+        users=users,
+        users_count=users_count,
+        playlists_count=playlists_count
     )
