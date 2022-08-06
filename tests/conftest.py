1ac9f6d143571ffd981e837dbf20d3681de7f7e1
Makar Timofeev <75175813+brintadis@users.noreply.github.com>
2022-08-06 18:27:23 +0300

    pylint appllied to the tests folder

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,12 +9,23 @@ from webapp import create_app
 
 @pytest.fixture(scope='module')
 def new_user():
-    user = User("test-user@example.com", "mypassword")
+    """
+    Create new user
+    """
+    email = "test-user@example.com"
+    password = "mypassword"
+    user = User(
+        email=email
+    )
+    user.set_password(password)
     return user
 
 
 @pytest.fixture(scope='module')
 def test_client():
+    """
+    Testing flask app client
+    """
     flask_app = create_app()
 
     with flask_app.test_client() as testing_client:
